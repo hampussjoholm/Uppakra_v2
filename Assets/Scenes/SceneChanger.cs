c8f5551ab0db23a6deea63fe589a6729e71e55b9
hampussjoholm <hampus.sjoholm@gmail.com>
2024-12-09 22:42:09 +0100

    Uploading existing working project

---
+++ Assets/Scenes/SceneChanger.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour {
+    public void ChangeScene() {
+        SceneManager.LoadScene("Uppakra_v2_scen2");
+    }
+}
