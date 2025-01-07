aebf2541fef4ec46600a4cce6eabd280f41cdc15
hampussjoholm <hampus.sjoholm@gmail.com>
2025-01-07 18:26:01 +0100

    Almost done
    kortade ner kap 2 och 3, men får vara så

--- Assets/Hampus_Assets/Scripts/SceneChanger.cs
+++ Assets/Hampus_Assets/Scripts/SceneChanger.cs
@@ -5,8 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour {
 
-    public string sceneName;
-    public void ChangeScene() {
-        SceneManager.LoadScene(sceneName);
+    public string nextSceneName;
+    public string prevSceneName;
+    public string lastSceneName;
+
+    public void NextScene() {
+        SceneManager.LoadScene(nextSceneName);
+    }
+
+    public void PrevScene() {
+        SceneManager.LoadScene(prevSceneName);
+    }
+
+    public void LastScene() {
+        SceneManager.LoadScene(lastSceneName);
     }
 }
